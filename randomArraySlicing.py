d49f9ed2b89bea76a027059d954dc650fa06b47c
DevParasar <58827285+DevParasar@users.noreply.github.com>
2021-04-18 19:23:07 +0530

    Add files via upload

---
+++ randomArraySlicing.py
@@ -0,0 +1,8 @@
+my_list = [1, 2, 3, 4, 5]
+
+print(my_list[2:])
+print(my_list[:2])
+print(my_list[2:4])
+print(my_list[::2])
+print(my_list[::-2])
+print(my_list[1:4:2])
---
+++ randomIndexOfAList.py
@@ -0,0 +1,4 @@
+my_list = [21, 44, 35, 11]
+
+for index, val in enumerate(my_list, start=0):
+    print(index, val)
